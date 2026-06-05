6d340649ed878d505e1dc48a637f4b269462957b
Dieu Bui <dieuhanu2008@gmail.com>
2026-06-05 16:56:57 +0700

    add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,11 @@
+let playerName = "Mario";
+let currentLives = 3;
+const arr = [25,30,45]
+let sum = arr[0] + arr[1] + arr[2]
+let trungbinh = sum/3
+console.log(`So coin bi du la ${trungbinh}`);
+
+//tinh tong coin cua 3 level, sau do chia 3
+// in ra so coin du khi chia tong so coin cho 3
+//commit ket qua vs message
+
