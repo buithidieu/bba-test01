6a4449ca7c93ddc00a9784ff4057dc0f40df7fc5
Dieu Bui <dieuhanu2008@gmail.com>
2026-06-05 17:49:33 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,14 @@
+let powerUp = "mushroom";
+let effect ="";
+
+if (powerUp === "mushroom"){
+    effect = "Mario becomes Super!"
+}
+    else if(powerUp === "flower"){
+    effect = "Mario can shoot fireballs!";
+    }
+    else if(powerUp = "star"){
+        effect= "Mario is invicible!";
+    }
+
+console.log(effect);
